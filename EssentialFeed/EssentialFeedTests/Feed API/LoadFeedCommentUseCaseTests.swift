b1f669e76cdb6250a1d4d08f8db9ebb7d2892184
Paul Lee <ctwdtw@gmail.com>
2021-03-07 15:42:25 +0800

    add `LoadImageCommentsFromRemoteUseCaseTests.swift`

---
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedCommentUseCaseTests.swift
@@ -0,0 +1,15 @@
+//
+//  LoadImageCommentsFromRemoteUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Paul Lee on 2021/3/7.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
+	func testCanFail() {
+		//XCTFail()
+	}
+}
